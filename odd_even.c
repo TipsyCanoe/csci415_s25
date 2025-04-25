cc9164a5ed3218fd95789765fcecadd6c8c7e9d7
TipsyCanoe <jefferr7@wwu.edu>
2025-04-25 10:54:54 -0700

    ensured odd_even was based on the example in class

--- odd_even.c
+++ odd_even.c
@@ -3,57 +3,54 @@
 #include <stdlib.h>
 #include <time.h>
 
-void odd_even_sort(int *local_data, int local_n, int rank, int size) {
-    int phase;
-    for (phase = 0; phase < size; phase++) {
-        if (phase % 2 == 0) {
-            if (rank % 2 == 0 && rank + 1 < size) {
-                MPI_Sendrecv(local_data, local_n, MPI_INT, rank + 1, 0,
-                             local_data, local_n, MPI_INT, rank + 1, 0,
-                             MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            } else if (rank % 2 == 1) {
-                MPI_Sendrecv(local_data, local_n, MPI_INT, rank - 1, 0,
-                             local_data, local_n, MPI_INT, rank - 1, 0,
-                             MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            }
-        } else {
-            if (rank % 2 == 1 && rank + 1 < size) {
-                MPI_Sendrecv(local_data, local_n, MPI_INT, rank + 1, 0,
-                             local_data, local_n, MPI_INT, rank + 1, 0,
-                             MPI_COMM_WORLD, MPI_STATUS_IGNORE);
-            } else if (rank % 2 == 0 && rank > 0) {
-                MPI_Sendrecv(local_data, local_n, MPI_INT, rank - 1, 0,
-                             local_data, local_n, MPI_INT, rank - 1, 0,
-                             MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+int main(int argc, char *argv[]) {
+    int rank, size;
+    int my_value;
+    double start, end;
+    // Initialize MPI
+    MPI_Init(&argc, &argv);
+    // Get the rank and size of the communicator
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    // Get the number of processes
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    // Check if the number of processes is even
+    srand(time(NULL) + rank);
+    // Initialize the value to be sorted
+    my_value = rand() % 1000;
+    MPI_Barrier(MPI_COMM_WORLD);
+    start = MPI_Wtime();
+    // start the odd-even sort
+    for (int phase = 0; phase < size; phase++) {
+        int partner;
+        // Determine the partner process for this phase
+        if (phase % 2 == 0) {  // Even phase
+            partner = (rank % 2 == 0) ? rank + 1 : rank - 1;
+        } else {               // Odd phase
+            partner = (rank % 2 == 0) ? rank - 1 : rank + 1;
+        }
+        // Ensure the partner is within bounds
+        if (partner < 0 || partner >= size)
+            partner = MPI_PROC_NULL;
+        // Send and receive values with the partner
+        int recv_value;
+        MPI_Sendrecv(&my_value, 1, MPI_INT, partner, 0,
+                     &recv_value, 1, MPI_INT, partner, 0,
+                     MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        // Compare and swap values
+        if (partner != MPI_PROC_NULL) {
+            if ((rank < partner && my_value > recv_value) ||
+                (rank > partner && my_value < recv_value)) {
+                my_value = recv_value;
             }
         }
     }
-}
-
-int main(int argc, char **argv) {
-    MPI_Init(&argc, &argv);
-
-    int rank, size;
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    MPI_Comm_size(MPI_COMM_WORLD, &size);
-
-    int n = 1000;
-    int local_n = n / size;
-    int *local_data = malloc(local_n * sizeof(int));
-    srand(time(NULL) + rank);
-
-    for (int i = 0; i < local_n; i++) {
-        local_data[i] = rand() % 1000;
-    }
-
-    double start = MPI_Wtime();
-    odd_even_sort(local_data, local_n, rank, size);
-    double end = MPI_Wtime();
-
+    //
+    MPI_Barrier(MPI_COMM_WORLD);
+    end = MPI_Wtime();
+    // Print the sorted value
     if (rank == 0)
-        printf("Odd-Even Sort completed in %f seconds\n", end - start);
+        printf("Odd-Even Sort completed in %f seconds.\n", end - start);
 
-    free(local_data);
     MPI_Finalize();
     return 0;
 }
